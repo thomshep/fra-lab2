bf17b0a0c48960231243f8ef58fea7dc03752762
Thomas Sheppard <thomasshep01@gmail.com>
2023-06-08 18:16:15 -0300

    codigo que solo muestra imagen mascara

---
+++ image_viewer.py
@@ -0,0 +1,18 @@
+import cv2, rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+def read_image_data(data):
+    print("entre")
+    try:
+        cv_image = CvBridge().imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+        print(e)
+    cv2.imshow("Video", cv_image)
+    cv2.waitKey(3)
+
+
+rospy.init_node('img_viewer')
+
+rospy.Subscriber("/mask", Image, read_image_data)
+rospy.spin()
