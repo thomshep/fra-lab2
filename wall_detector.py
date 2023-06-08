8cd411a72434856da9739f35100f13938384d8b8
Thomas Sheppard <thomasshep01@gmail.com>
2023-06-07 22:44:20 -0300

    wall detector con segmentos

--- wall_detector.py
+++ wall_detector.py
@@ -15,20 +15,23 @@ esta_girando = False
 pixeles_a_evaluar_fila = 8
 altura_maxima_pared = 500
 
+minimo_largo_segmento = 100
+margen_superior = 250
+maximo_tamano_pendiente = 0.1
 
-def girar():
-    global esta_girando
+# def girar():
+#     global esta_girando
 
-    twist = Twist()
-    twist.angular = Vector3(0,0,1)
-    motor_pub.publish(twist)
+#     twist = Twist()
+#     twist.angular = Vector3(0,0,1)
+#     motor_pub.publish(twist)
 
-    time.sleep(5.5)
+#     time.sleep(5.5)
 
-    esta_girando = False
+#     esta_girando = False
 
 def read_image_data(data):
-    global esta_girando
+    global esta_girando, margen_superior, minimo_largo_segmento, maximo_tamano_pendiente
     
     if not esta_girando:
         print("entre")
@@ -39,7 +42,7 @@ def read_image_data(data):
 
         frame_HSV = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(frame_HSV, (0,0, 220), (30,30, 255))
+        mask = cv2.inRange(frame_HSV, (0,0, 220), (30,190, 255))
 
         frame_RGB = cv2.bitwise_and(cv_image,cv_image,mask = mask)
 
@@ -54,35 +57,99 @@ def read_image_data(data):
 
         print("kkkk")
 
-        centro_eje_x_imagen = math.floor(frame_RGB.shape[1] / 2)
+        # ####### USANDO PUNTOS BLANCOS
+        
+        # centro_eje_x_imagen = math.floor(frame_RGB.shape[1] / 2)
         
 
-        for indice_fila, fila in enumerate(frame_RGB):
-            cantidad_blancos = 0
-            if indice_fila > altura_maxima_pared:
-                break
-            #revisar las 6 columnas adyacentes a la izquierda
-            for indice_columna in range(centro_eje_x_imagen - math.floor(pixeles_a_evaluar_fila / 2), centro_eje_x_imagen + math.floor(pixeles_a_evaluar_fila / 2)):
-                pixel = frame_RGB[-1 - indice_fila][indice_columna]
-                #hay color blanco
-                if(pixel[0] > 100 and pixel[1] > 100 and pixel[2] > 100):
-                    cantidad_blancos += 1
+
+        # for indice_fila, fila in enumerate(frame_RGB):
+        #     cantidad_blancos = 0
+        #     if indice_fila > altura_maxima_pared:
+        #         break
+        #     #revisar las 6 columnas adyacentes a la izquierda
+        #     for indice_columna in range(centro_eje_x_imagen - math.floor(pixeles_a_evaluar_fila / 2), centro_eje_x_imagen + math.floor(pixeles_a_evaluar_fila / 2)):
+        #         pixel = frame_RGB[-1 - indice_fila][indice_columna]
+        #         #hay color blanco
+        #         if(pixel[0] > 100 and pixel[1] > 100 and pixel[2] > 100):
+        #             cantidad_blancos += 1
                     
-                    print(cantidad_blancos)
-                    #para salir del otro for
-                    if cantidad_blancos > pixeles_a_evaluar_fila / 2:
-                        esta_girando = True
+        #             print(cantidad_blancos)
+        #             #para salir del otro for
+        #             if cantidad_blancos > pixeles_a_evaluar_fila / 2:
+        #                 esta_girando = True
                         
-                        print(indice_fila)
-                        print(frame_RGB.shape)
+        #                 print(indice_fila)
+        #                 print(frame_RGB.shape)
                     
 
-                        girar()
-                        return
+        #                 girar()
+        #                 return
+
+        # ####### USANDO SEGMENTOS DE RECTA
+
+        edges = cv2.Canny(mask, 200, 400)
+        #recortar parte de arriba imagen?
+        cropped_edges = cv2.bitwise_and(cv_image,cv_image,mask=edges)
+        #cv2.imshow("Video", cropped_edges)
+        imagen_edges_gris = cv2.cvtColor(cropped_edges, cv2.COLOR_BGR2GRAY)
+
+        frame_RGB = cropped_edges
+
+        def detect_line_segments(cropped_edges):
+            # tuning min_threshold, minLineLength, maxLineGap is a trial and error process by hand
+            rho = 1  # distance precision in pixel, i.e. 1 pixel
+            angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+            min_threshold = 30  # minimal of votes
+            line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold, 
+                                            np.array([]), minLineLength=15, maxLineGap=20)
+            if line_segments is None:
+                return []
+
+            return line_segments
         
+
+        def region_of_interest(matriz, altura_maxima):
+            submatriz = []
+            for fila in matriz:
+                submatriz.append(fila[len(fila) - altura_maxima:])
+            return submatriz
+
+        def draw_line_segments(image, line_segments):
+            for line in line_segments:
+                x1, y1, x2, y2 = line[0]
+                print("draw line")
+                image = cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Dibuja una línea verde de grosor 2
+            return image
+
+        segmentos = detect_line_segments(imagen_edges_gris)
+
+        def filtrado_segmentos(segmento):
+            x1, y1, x2, y2 = segmento[0]
+            pendiente = (y2 - y1) / (x2 - x1)
+
+            return abs(pendiente) < maximo_tamano_pendiente and y1 > margen_superior and x2 - x1 > minimo_largo_segmento
+        
+        print("len(segmentos)")
+        segmentos = list(filter(filtrado_segmentos, segmentos))
+
         twist = Twist()
-        twist.linear = Vector3(0.1,0,0)
-        motor_pub.publish(twist)
+
+        if len(segmentos) > 0: #frenar
+            print("frenar")
+            print(segmentos)
+            motor_pub.publish(twist)
+        else:
+            #Avanzar
+            print("avanzar")
+            twist.linear = Vector3(0.1,0,0)
+            motor_pub.publish(twist)
+
+
+        imagen_edges_a_color = cv2.cvtColor(imagen_edges_gris, cv2.COLOR_GRAY2BGR)
+        imagen_edges_a_color = draw_line_segments(imagen_edges_a_color, segmentos)
+        cv2.imshow("Video", imagen_edges_a_color)
+        cv2.waitKey(3)
 
         
 
