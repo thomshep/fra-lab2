ada554c1952c397a7f5e2fe89c6e246572e6eb47
guillermowaltes <guillermowaltes>
2023-06-22 21:37:47 -0300

    se genera un nodo ficticio para simular decisiones de giro

---
+++ publish_giro.py
@@ -0,0 +1,24 @@
+import rospy
+from std_msgs.msg import String, Bool
+from geometry_msgs.msg import Twist, Vector3
+import random
+import threading
+import time
+
+rospy.init_node('samples')
+
+giro_pub = rospy.Publisher("/navegacion/giro", String, queue_size=20)
+
+def publicar_giro():
+    decision = random.randint(0,1)
+    if decision == 1:
+        print('decide der')
+        giro_pub.publish("der")
+    else:
+        print('decide izq')
+        giro_pub.publish("izq")
+
+while True:
+    publicar_giro()
+    time.sleep(30)
+
