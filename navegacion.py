fca604f11ddcde06a9540a2960bb0dadbf8da8cf
guillermowaltes <guillermowaltes>
2023-06-23 11:07:07 -0300

    ajustamos distancia maxima

--- navegacion.py
+++ navegacion.py
@@ -35,7 +35,7 @@ def camino_mas_corto(G, nodo_inicial, nodo_final):
     print(f"Camino: {shortest_path} => Distancia: {shortest_distance}")
 
 
-DISTANCIA_MAXIMA_NODOS_CERCA = 4
+DISTANCIA_MAXIMA_NODOS_CERCA = 10
 
 #define orientaciones de signo segun
 #E,O,N,S
